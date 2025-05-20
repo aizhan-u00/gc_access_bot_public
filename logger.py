59538c54018432742fd7dccb3e5ac18942008d6c
aizhan-u00 <aizhan.uristembek@gmail.com>
2025-05-20 17:01:48 +0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -7,23 +7,23 @@ Supports DEBUG (file) and INFO (console) logging levels with formatted output.
 
 import logging
 
-# Создаем логгер
-logger = logging.getLogger("access-bot")  # Задаем имя логгера
-logger.setLevel(logging.DEBUG)  # Уровень логирования
+# Create the logger
+logger = logging.getLogger("access-bot")  # Set the logger name
+logger.setLevel(logging.DEBUG)  # Logging level
 
-# Формат логов
+# Log format
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-# Логирование в файл
+# Logging to file
 file_handler = logging.FileHandler("access-bot.log")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
-# Логирование в консоль
+# Logging to console
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 console_handler.setFormatter(formatter)
 
-# Добавляем обработчики
+# Add handlers
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
